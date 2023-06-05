4e5f7a9d1deeea46f331542aaeb2babb194b7f52
Serquand <esteban.vincent@efrei.net>
2023-06-05 15:14:15 +0200

    Pong

--- Server/src/commands/Ping.ts
+++ Server/src/commands/Ping.ts
@@ -2,8 +2,11 @@ import { Client, Interaction } from "discord.js";
 
 module.exports = {
     name: "ping", 
-    description: "Ping the user", 
-    runSlash: (client: Client, interaction: Interaction) => {
-        interaction.channel?.send("Hello !");
+    description: "Answer by Pong !", 
+    runSlash: (client: Client, interaction: any) => {
+        interaction.reply({
+            content: "Pong !", 
+            ephemeral: true 
+        });
     }
 }
