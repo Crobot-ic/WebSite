eaf4f58189b874d54241440178424b0f3944c729
Serquand <esteban.vincent@efrei.net>
2023-08-06 19:28:33 +0200

    Commitment

--- Web/src/components/Home/Service.jsx
+++ Web/src/components/Home/Service.jsx
@@ -1,7 +1,14 @@
 const Service = (props) => {
     return (
-        <h1>Service</h1>
+        <div class="service-container service-container-visible">
+            <div class="service">
+                <div class="content"></div> 
+                <div class="overlay"></div>
+                <img class="title-service" src="title" />
+            </div>
+            <h4 class="description">Test</h4>
+        </div>
     );
 }
 
-export default Service
+export default Service;
