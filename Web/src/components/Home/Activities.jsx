f937bd6c56a3b0c28a369fa7285c85689130834a
Serquand <esteban.vincent@efrei.net>
2023-08-07 23:59:09 +0200

    Activites title app on discover

--- Web/src/components/Home/Activities.jsx
+++ Web/src/components/Home/Activities.jsx
@@ -1,13 +1,36 @@
 import "../../styles/css/index.css";
 import informations from "../../../informations.json";
 import Activity from "./Activity";
+import { useRef, useState, useEffect } from "react";
 
 const Activities = () => {
     const { activities } = informations;
+    const [isIntersected, setIsIntersected] = useState(false);
+    const [isAlreadyIntersected, setIsAlreadyIntersected] = useState(false);
+    const componentRef = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+            setIsIntersected(entry.isIntersecting);
+        }, { threshold: 1 });
+        observer.observe(componentRef.current);
+    
+    }, []);
+    
+    useEffect(() => {
+        if (isIntersected && !isAlreadyIntersected) {
+            setIsAlreadyIntersected(true);
+        }
+    }, [isIntersected]);
 
     return (
         <section className="activities">
-            <h2 className="activities-title">Nos activités</h2>
+            <div 
+                className={`activities-title ${isAlreadyIntersected ? "activities-title-visible" : ""}`}
+                ref={componentRef}
+            >
+                <h2 className="activities-title-content">Nos activités</h2>
+            </div>
             <div className="activities-content">
                 {activities.map((activity, index) => (
                     <Activity 
