fb45a2ec211cfe11173cb9d6bc9dfd37499a6ffb
Serquand <esteban.vincent@efrei.net>
2023-08-07 18:29:18 +0200

    Main Page

--- Web/src/components/Home/MainPage.jsx
+++ Web/src/components/Home/MainPage.jsx
@@ -3,7 +3,13 @@ import "../../styles/css/index.css";
 const MainPage = () => {
     return (
         <section className="main-page">
-            
+            <div className="main-page-info">
+                <div className="main-page-info-title">
+                    <h1 className="main-page-info-title-content">Crobot'ic</h1>
+                </div>
+                <h3 className="main-page-info-slogan">L'association de robotique de l'école Efrei Paris</h3>
+            </div>
+            <div className="main-page-sep"></div>
         </section>
     )
 }
