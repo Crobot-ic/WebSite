aa8acab684cee7a6be71fab57f7e5274c0cd4a03
Serquand <esteban.vincent@efrei.net>
2023-08-07 22:38:22 +0200

    IntersectionObserver on HomeNav

--- Web/src/components/Home/HomeNavChoice.jsx
+++ Web/src/components/Home/HomeNavChoice.jsx
@@ -1,15 +1,46 @@
 import "../../styles/css/index.css";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 
 const HomeNavChoice = (props) => {
-    const { children, titleNav, buttonInfo, goTo } = props;
+    const homeNavRef = useRef();
+    const [isIntersecting, setIsIntersecting] = useState(false);
+    const [isAlreadyIntersecting, setIsAlreadyIntersecting] = useState(false);
+    const { children, titleNav, buttonInfo, goTo, transitionInit } = props;
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+            setIsIntersecting(entry.isIntersecting)
+        }, { threshold: 1, rootMargin: "20px" });
+        observer.observe(homeNavRef.current);
+    }, []);
+
+    useEffect(() => {
+        if (isIntersecting) {
+            setIsAlreadyIntersecting(true);
+        }
+    }, [isIntersecting]);
+
     return (
-        <div className="home-nav">
-            <h2 className="home-nav-title">{ titleNav }</h2>
-            { children }
-            <button className="home-nav-button" onClick={() => navigate(goTo)} >{ buttonInfo }</button>
+        <div 
+            className={`home-nav ${isAlreadyIntersecting ? "home-nav-visible" : ""}`}
+            ref={homeNavRef}
+        >
+            <h2 
+                className="home-nav-title"
+                style={{ transform: `translateX(${transitionInit}px)` }}
+            >{ titleNav }</h2>
+            <div 
+                style={{ transform: `translateX(${transitionInit}px)` }}
+                className="home-nav-text"
+            >
+                { children }
+            </div>
+            <button 
+                className="home-nav-button" 
+                onClick={() => navigate(goTo)}
+            >{ buttonInfo }</button>
         </div>
     );
 }
--- Web/src/components/Home/HomeNavigation.jsx
+++ Web/src/components/Home/HomeNavigation.jsx
@@ -3,12 +3,24 @@ import HomeNavChoice from "./HomeNavChoice";
 
 const HomeNavigation = () => {
     return (
-        <section className="home-navigation">
-            <HomeNavChoice titleNav="Nos projets" buttonInfo="Découvez-les !" goTo="/projects">
-                <p className="home-nav-text">L'association possède d'innombrables projets tous aussi intéressants les uns que les autres !</p>
+        <section className="home-navigation">    
+            <HomeNavChoice 
+                titleNav="Nos projets" 
+                buttonInfo="Découvez-les !" 
+                goTo="/projects"
+                transitionInit={-50}
+            >
+                <p className="home-nav-text">
+                    L'association possède d'innombrables projets tous aussi intéressants les uns que les autres !
+                </p>
             </HomeNavChoice>
 
-            <HomeNavChoice titleNav="Nos événements" buttonInfo="Découvez-les !" goTo="/events">
+            <HomeNavChoice 
+                titleNav="Nos événements" 
+                buttonInfo="Découvez-les !" 
+                goTo="/events"
+                transitionInit={50}
+            >
                 <p className="home-nav-text">
                     Notre engagement : un événement minimum par semaine !<br />
                     Avancement de projets, découverte technologique, jeux, cohésion, tellement de possibilités !<br />
