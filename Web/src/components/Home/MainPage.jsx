b589d0befe24f1cc2ca1a5914cba01eb067e1517
Serquand <esteban.vincent@efrei.net>
2023-09-11 05:42:00 +0200

    Home page - Responsive

--- Web/src/components/Home/MainPage.jsx
+++ Web/src/components/Home/MainPage.jsx
@@ -9,7 +9,6 @@ const MainPage = () => {
                 </div>
                 <h3 className="main-page-info-slogan">L'association de robotique de l'école Efrei Paris</h3>
             </div>
-            <div className="main-page-sep"></div>
         </section>
     )
 }
