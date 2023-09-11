af01da620093ce48c1bc5e720bacbef93b4f1198
Serquand <esteban.vincent@efrei.net>
2023-09-11 23:41:32 +0200

    List Project - Ended

--- Server/src/Discord/commands/Projects/ListProjects.ts
+++ Server/src/Discord/commands/Projects/ListProjects.ts
@@ -1,7 +1,21 @@
 import { CommandInteraction, Client } from "discord.js";
+import Project from "../../../Models/Project";
 
-const listProjects = (client: Client, interaction: CommandInteraction) => {
-    return interaction.reply("Test")
+const listProjects = async (client: Client, interaction: CommandInteraction) => {
+    const allProjects = await Project.findAll({ attributes: ["projectName", "projectId"] });
+    let message = "";
+
+    for(let i = 0; i < allProjects.length; i++) {
+        message += 
+            "**Nom du projet :** " + allProjects[i].dataValues.projectName + "  -  " +
+            "**ID du projet :** " + allProjects[i].dataValues.projectId + "\n";
+    }
+
+    if(message == "") {
+        message = "À l'heure actuelle, l'association n'a aucun projet !";
+    }
+
+    return interaction.reply({ content: message, ephemeral: true });
 }
 
 export default listProjects;
